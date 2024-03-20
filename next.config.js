4ccbcbad2bb83fc6115e6c3d05b188489d41f870
=Natty-7r <nati7fekadu@gmail.com>
2024-03-20 09:19:06 +0300

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  webpack(config, options) {
-    // Add your custom configuration here
-    config.resolve.alias["@pages"] = path.join(__dirname, "src", "pages");
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
