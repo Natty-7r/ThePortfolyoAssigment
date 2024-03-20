fef26dc6b3391f1b1ae9cea13dfe0aca8bbd36fb
=Natty-7r <nati7fekadu@gmail.com>
2024-03-20 09:21:10 +0300

    fix: resolve deployment issues

--- src/pages/api/get-data.js
+++ src/pages/api/get-data.js
@@ -1,8 +1,8 @@
-import { apiURL } from "@/utils/constants/api";
+import { API_URL } from "@/src/utils/contants/api";
 
 export default async function handler(req, res) {
   try {
-    const respose = await fetch(apiURL);
+    const respose = await fetch(API_URL);
     const { success, user } = await respose.json();
     if (success) {
       return res.status(200).json({ success: true, user });
--- src/utils/contants/api.js
+++ src/utils/contants/api.js
@@ -1,2 +1,2 @@
-export const apiURL =
+export const API_URL =
   "https://dashboard-backend.cyclic.app/api/v1/get/user/65b3a22c01d900e96c4219ae";
